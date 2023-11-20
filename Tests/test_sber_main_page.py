957235e7e6aad7652bb55bc8425b214b3e077597
denis <denisofflive@mail.ru>
2023-11-20 14:49:47 +0300

    Поиск элементов в коде (Москва, ENG, поиск-лупа)

--- Tests/test_sber_main_page.py
+++ Tests/test_sber_main_page.py
@@ -1,6 +1,6 @@
 import time
-import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 def test_open_sber_main_page():
@@ -9,4 +9,14 @@ def test_open_sber_main_page():
     driver.implicitly_wait(10)
     driver.get("http://www.sberbank.ru/")
     driver.maximize_window()
+
+    # Геометка Москва
+    driver.find_element(By.XPATH, "//a[text()='Москва']")
+    print("Москва")
+    # Переключение языка "ENG"
+    driver.find_element(By.XPATH, "(//a[text()='ENG'])[1]")
+    print("ENG")
+    # Меню "поиск-лупа"
+    driver.find_element(By.XPATH, "//a[@aria-label='Открыть поиск по сайту']")
+    print("поиск-лупа")
     time.sleep(3)
