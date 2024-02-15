fb4a2b23fb0239ae38c2f56e9eb35432bcb3fc92
denis <denisofflive@mail.ru>
2024-02-15 16:38:18 +0300

    Добавил фикстуры для работы с браузерами Edge, Firefox

--- conftest.py
+++ conftest.py
@@ -2,12 +2,36 @@ import pytest
 from selenium import webdriver
 
 
+# Описываем дополнительные опции командой строки
+# --bro - браузер, на котором будет выполняться тестирование
+
+def pytest_addoption(parser):
+    parser.addoption("--bro",
+                     action="store",
+                     default="Chtome",
+                     help="available browsers: Edge, Firefox, Chrome",
+                     choices=("Edge", "Firefox", "Chrome")
+                     )
+
+# Фикстура браузера
+@pytest.fixture(autouse=True, scope="session")
+def bro(pytestconfig):
+    bro = pytestconfig.getoption("bro")
+    yield bro
+
 # Фикстура для запуска браузера Google Chrome
 @pytest.fixture(scope="session")
-def browser():
+def browser(bro):
     try:
         # Инициализируем браузер
-        driver = webdriver.Chrome()
+        if bro == "Chrome":
+            driver = webdriver.Chrome()
+        elif bro == "Firefox":
+            driver = webdriver.Firefox()
+        elif bro == "Edge":
+            driver = webdriver.Edge()
+        else:
+            raise ValueError("Invalid browser name")
         yield driver
     finally:
         # Закрытие браузера
